4724083ee6f0c30f27ae4c4a33094bf35ec953bb
Hartik Salaria <salariahartik34@mail.com>
2022-09-25 11:20:26 +0530

    bug fix

--- graph/djikstra.cpp
+++ graph/djikstra.cpp
@@ -1,73 +1,65 @@
 #include <bits/stdc++.h>
-#define int long long int 
+#define int long long int
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-    #include "debug.h"
+#ifndef LOCAL
+#include "debug.h"
 #else
-    #define debug(x)
+#define debug(x)
 #endif
-
 
 const int inf = 1e18;
 
 class Graph {
-
-private:    
+   private:
     int V;
-    vector <pair <int, int>> *gr;
+    vector<pair<int, int>> *gr;
 
-public:
-
-    Graph(int V)    {
+   public:
+    Graph(int V) {
         this->V = V;
-        gr = new vector <pair <int, int>> [V];
+        gr = new vector<pair<int, int>>[V];
     }
 
-    void addEdge(int x, int y, int w)  {
+    void addEdge(int x, int y, int w) {
         gr[x].push_back(make_pair(y, w));
         gr[y].push_back(make_pair(y, w));
     }
 
-   vector<int> shortestPath(int s)   {
-
+    vector<int> shortestPath(int s) {
         int nodes = V;
         vector<int> dist(nodes, inf);
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
         dist[s] = 0;
         q.push({0, s});
-        while(!q.empty()){
+        while (!q.empty()) {
             auto [dis, node] = q.top();
             q.pop();
-            if(dis != dist[node]) continue;
-            for(auto [y, w]: gr[node]){
-                if(dist[y] > dis + w){
+            if (dis != dist[node]) continue;
+            for (auto [y, w] : gr[node]) {
+                if (dist[y] > dis + w) {
                     dist[y] = dis + w;
                     q.emplace(dist[y], y);
                 }
             }
         }
         return dist;
-
-   }
-
+    }
 };
 
-
-int32_t main()  {
-
+int32_t main() {
     int n, m;
     cin >> n >> m;
 
     Graph g(n);
-    while(m--)  {
+    while (m--) {
         int x, y, w;
         cin >> x >> y >> w;
         g.addEdge(x, y, w);
     }
 
     auto v = g.shortestPath(0);
-    
+
     debug(v);
 
     return 0;
