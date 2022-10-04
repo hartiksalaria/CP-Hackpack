3bafbd9bb15c9fa9943f37afb3643ec457e85c18
Hartik Salaria <salariahartik34@mail.com>
2022-10-04 11:31:17 +0530

    added refereces for trie

--- strings/trie.cpp
+++ strings/trie.cpp
@@ -1,76 +1,70 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define endl "\n"
 #define int long long int
 
-#ifndef ONLINE_JUDGE
-  #include "debug.h"
+#ifdef LOCAL
+#include "debug.h"
 #else
-  #define debug(x)
+#define debug(x)
 #endif
 
-
 class Node {
-public:
-  char data;
-  bool isTerminal;
-  vector<Node *> child;
+   public:
+    char data;
+    bool isTerminal;
+    vector<Node *> child;
 
-  Node(char d) {
-      data = d;
-      isTerminal = false;
-      child.assign(26, NULL);
-  }
-
+    Node(char d) {
+        data = d;
+        isTerminal = false;
+        child.assign(26, NULL);
+    }
 };
 
 class Trie {
-public:
-  Node *root;
-  
-  Trie()  {
-      root = new Node('\0');
-  }
-  
-  void insert(string word) {
-    Node *t = root;
-    for(char ch:word) {
-        if (!t->child[ch - 'a'])  {
-            Node *tmp = new Node(ch);
-            t->child[ch - 'a'] = tmp;
+   public:
+    Node *root;
+
+    Trie() {
+        root = new Node('\0');
+    }
+
+    void insert(string &word) {
+        Node *t = root;
+        for (char &ch : word) {
+            if (!t->child[ch - 'a']) {
+                Node *tmp = new Node(ch);
+                t->child[ch - 'a'] = tmp;
+            }
+            t = t->child[ch - 'a'];
         }
-        t = t->child[ch - 'a'];
+
+        t->isTerminal = true;
     }
 
-      t->isTerminal = true;
-
-  }
-
-bool search(string word)  {
-
-    Node *t = root;
-    for(char ch:word) {
-      if(t->child[ch - 'a'])
-        t = t->child[ch - 'a'];
-      else 
-        return false;
+    bool search(string &word) {
+        Node *t = root;
+        for (char &ch : word) {
+            if (t->child[ch - 'a'])
+                t = t->child[ch - 'a'];
+            else
+                return false;
+        }
+        return t->isTerminal;
     }
-    return t->isTerminal;
-  }
-
 };
 
-
-int32_t main()  {
-
+int32_t main() {
     Trie t;
-    
-    vector <string> s = {"hello", "he", "apple", "apes", "news"};
 
-    for(string word:s)
-      t.insert(word);
+    vector<string> s = {"hello", "he", "apple", "apes", "news"};
 
-    cout << t.search("hel"); 
+    for (string word : s)
+        t.insert(word);
+
+    string tmp = "hel";
+
+    cout << t.search(tmp);
 
     return 0;
 }
