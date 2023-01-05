12c552b03d0858524eb744e694d8d08f1c3ae1bb
Hartik Salaria <salariahartik34@mail.com>
2023-01-06 00:19:44 +0530

    efficient dsu with component count

--- graph/DSU2.cpp
+++ graph/DSU2.cpp
@@ -1,22 +1,23 @@
 // https://codeforces.com/contest/1332/problem/C
+// https://codeforces.com/contest/1768/problem/D
 
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long 
-#define endl "\n"
 
-#ifndef ONLINE_JUDGE
+#ifdef LOCAL
 #include "debug.h"
 #else
-#define debug(...)
+#define debug(...) 42
 #endif
 
 struct DSU {
+    int components;
     vector<int> parent, rank;
     DSU(int n) {
+        components = n;
         parent.assign(n, -1);
         rank.assign(n, 1);
-    }  
+    }
     int get(int a) {
         if (parent[a] == -1)
             return a;
@@ -25,10 +26,11 @@ struct DSU {
     void merge(int a, int b) {
         int s1 = get(a);
         int s2 = get(b);
-        if (s1 == s2) 
+        if (s1 == s2)
             return;
-        if (rank[s1] < rank[s2]) 
+        if (rank[s1] < rank[s2])
             swap(s1, s2);
+        components--;
         parent[s2] = s1;
         rank[s1] += rank[s2];
     }
@@ -37,36 +39,40 @@ struct DSU {
     }
 };
 
-int32_t main() {
+int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+    cin.tie(0);
 
     int tt;
     cin >> tt;
 
     while (tt--) {
-        int n, k;
-        cin >> n >> k;
-        string s;
-        cin >> s;
-        DSU t(n);
-        for (int i = 0; i < k; ++i)
-            for (int j = i + k; j < n; j += k) 
-               t.merge(i, j);
-        for (int i = 0; i < n / 2; ++i) 
-            t.merge(i, n - 1 - i);
-        map<int, map<char, int>> f;
-        for (int i = 0; i < n; ++i) 
-            f[t.get(i)][s[i]]++;
-        int ans = 0;
-        for (auto &[x, _f]: f) {
-            int sz = t.sz(x);
-            int mx = 0;
-            for (auto [_, __f]: _f)
-                mx = max(mx, __f);
-            ans += sz - mx;
+        int n;
+        cin >> n;
+        vector<int> p(n), pos(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> p[i];
+            p[i]--;
+            pos[p[i]] = i;
         }
-        cout << ans << endl;
+        DSU s(n);
+        for (int i = 0; i < n; ++i) {
+            if (p[i] != i) {
+                s.merge(p[i], pos[p[i]]);
+            }
+        }
+
+        int inv = n - s.components;
+        int ans = 1e9;
+        for (int i = 1; i < n; ++i) {
+            if (s.get(i) == s.get(i - 1)) {
+                ans = min(ans, inv - 1);
+            } else {
+                ans = min(ans, inv + 1);
+            }
+        }
+
+        cout << ans << '\n';
     }
 
     return 0;
